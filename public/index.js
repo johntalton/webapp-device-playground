b8542e18f0c85eeb9e2441c32896bea7e0ad633a
johntalton <johntalton@gmail.com>
2023-04-27 22:01:03 -0400

    theme rework

--- public/index.js
+++ public/index.js
@@ -195,18 +195,19 @@ async function hydrateCustomElements() {
 
 async function hydrateEffects() {
 	return
-
 	setInterval(() => {
 		const root = document.querySelector(':root')
 		console.log(root.style)
-		const currentStr = root.style.getPropertyValue('--color-accent--h')
+		// const currentStr = root.style.getPropertyValue('--color-accent--h')
 
-		console.log({ currentStr })
-		const current = currentStr === '' ? 180 : parseInt(currentStr)
-		const next = (current === NaN ? 180 : current) + ((Math.random() > 0.5) ? -2 : 3)
+		// console.log({ currentStr })
+		// const current = currentStr === '' ? 180 : parseInt(currentStr)
+		// const next = (current === NaN ? 180 : current) + ((Math.random() > 0.5) ? -20 : 30)
+
+		const next = Math.trunc(Math.random() * 360)
 
 		root.style.setProperty('--color-accent--h', next)
-	}, 1000)
+	}, 1000 * 7)
 }
 
 //
