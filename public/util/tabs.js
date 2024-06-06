557082edfb73f0e3d06c0fc1215aef4e3d39d741
johntalton <johntalton@gmail.com>
2024-06-06 09:26:44 -0400

    tab cleanup

--- public/util/tabs.js
+++ public/util/tabs.js
@@ -5,26 +5,27 @@ export function bindTabRoot(root) {
       event.preventDefault()
 
       const { target} = event
-			// const parent = target?.parentNode
-			// const grandParent = parent.parentNode
-      const parent = root // target?.parentNode.parentNode
+			const parent = target?.parentNode
+			const grandParent = parent.parentNode
 
       const tabName = target.getAttribute('data-tab')
 
       // remove content active
-      const activeOthers = parent.querySelectorAll(':scope > [data-active]')
+      const activeOthers = grandParent.querySelectorAll(':scope > [data-active]')
       activeOthers.forEach(ao => ao.toggleAttribute('data-active', false))
 
       // remove tab button active
       const activeOthersTabsButtons = parent.querySelectorAll(':scope > button[data-tab]')
       activeOthersTabsButtons.forEach(ao => ao.toggleAttribute('data-active', false))
 
-      const tabContentElem = parent.querySelector(`:scope > [data-for-tab="${tabName}"]`)
+      const tabContentElem = grandParent.querySelector(`:scope > [data-for-tab="${tabName}"]`)
       if(tabContentElem === null) { console.warn('tab content not found', tabName) }
       else {
+        // set content active
         tabContentElem.toggleAttribute('data-active', true)
       }
 
+      // set self active
       tabButton.toggleAttribute('data-active', true)
     })
   }
